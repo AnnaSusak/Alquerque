2ad4948877a04aaa5d63e11821683ecb46dd2bac
Аня Сусак <anna.i.susak@gmail.com>
2023-10-30 18:18:30 +0300

    add net_lib project

---
+++ AlquerqueApp/NetLib/BasicNetMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+
+namespace NetLib
+{
+    public class BasicNetMethods
+    {
+        /// <summary>
+        /// Отправка текста по сети
+        /// <param name="socket"></param>
+        /// <param name="=text"></param>
+        /// </summary>
+        public static void SendDataToNet(Socket socket, string text)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(text);
+            socket.Send(bytes);
+        }
+        public static string ReadDateFromNet(Socket socket)
+        {
+            //буфер приема
+            byte[] data = new byte[256];
+            int len = socket.Receive(data);
+            return Encoding.Unicode.GetString(data, 0, len);
+        }
+    }
+}
