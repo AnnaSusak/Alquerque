f758d21e37284fbf7d535bd90836aca7f557419c
Аня Сусак <anna.i.susak@gmail.com>
2023-11-06 15:55:13 +0300

    fix arranging bug

--- AlquerqueApp/Bot/MainWindow.xaml.cs
+++ AlquerqueApp/Bot/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Windows.Threading;
+using System.Runtime.ConstrainedExecution;
 
 namespace Bot
 {
@@ -35,10 +36,20 @@ namespace Bot
     {
         public int ind_pos;
         public int ind_but;
-        public InfoForMove(int pos, int ind)
+        public bool eat;
+        public int other_but_ind;
+        public InfoForMove(int pos, int ind, bool eat)
         {
             ind_pos = pos;
             ind_but = ind;
+            this.eat = eat;
+        }
+        public InfoForMove(int pos, int ind, bool eat, int ind_other)
+        {
+            ind_pos = pos;
+            ind_but = ind;
+            this.eat = eat;
+            other_but_ind = ind_other;
         }
     }
     public partial class MainWindow : Window
@@ -72,7 +83,7 @@ namespace Bot
             var directory = FilePath.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             field.Source = new BitmapImage(new Uri(FilePath.Combine(directory, "Resourses/playing_field.jpg")));
             ArrangeButtons(509, 65, true, -width_step, height_step);
-            ArrangeButtons(227, 360, false, width_step, -height_step);
+            ArrangeButtons(237, 345, false, width_step, -height_step);
             empty_pos.Add(new Position(373, 205));
             // подготовка поля для отображения текста
             PrepareGameInfo();
@@ -88,6 +99,7 @@ namespace Bot
                 Button b = new Button();
                 b.Width = BUTTON_SIZE;
                 b.Height = BUTTON_SIZE;
+                b.Foreground = Brushes.Cyan;
                 b.IsEnabled = false;
                 b.Content = (i - 1).ToString();
                 Canvas.SetLeft(b, l);
@@ -180,7 +192,7 @@ namespace Bot
                                 if (my_buttons.Count != 0)
                                     await FindPossibleButtonIndsForMove();
                                 //   MessageBox.Show("found");
-                                MessageBox.Show(enabled_moves.Count.ToString());
+                             //   MessageBox.Show(enabled_moves.Count.ToString());
                                 if (enabled_moves.Count == 0 || my_buttons.Count== 0)
                                 {
                                     MessageBox.Show("Вы проиграли");
@@ -189,7 +201,7 @@ namespace Bot
                                 } else
                                 {
                                     int mv = r.Next(0, enabled_moves.Count - 1);
-                                    await MakeMove(enabled_moves[mv].ind_but, enabled_moves[mv].ind_pos, true);
+                                    await MakeMove(enabled_moves[mv], true);
                                     message = "";
                                     Lib.BasicNetMethods.SendDataToNet(client_socket, enabled_moves[mv].ind_but + " " +
                                         enabled_moves[mv].ind_pos.ToString());
@@ -211,7 +223,7 @@ namespace Bot
                        // MessageBox.Show(ind + " " + ind_empty);
                         string a = "r";
                         if (my_buttons == buttons_red) { a = "b"; }
-                        await MakeMove(ind, ind_empty, false);
+                        await MakeMove(new InfoForMove(ind_empty, ind, false), false);
                         message = "";
                     }
                     if (prev_m != message)
@@ -238,28 +250,46 @@ namespace Bot
                 isFinished = true;
             }
         }
-        async Task MakeMove (int ind_of_button, int ind_of_empty_pos, bool my)
+        async Task MakeMove (InfoForMove inf, bool my)
         {
-            double a = empty_pos[ind_of_empty_pos].x;
-            double b = empty_pos[ind_of_empty_pos].y;
-            await Dispatcher.InvokeAsync(() =>
+            try
             {
-                if (my)
+                int ind_of_empty_pos = inf.ind_pos;
+                int ind_of_button = inf.ind_but;
+                double a = empty_pos[ind_of_empty_pos].x;
+                double b = empty_pos[ind_of_empty_pos].y;
+                await Dispatcher.InvokeAsync(() =>
                 {
-                    empty_pos[ind_of_empty_pos].x = Canvas.GetLeft(my_buttons[ind_of_button]);
-                    empty_pos[ind_of_empty_pos].y = Canvas.GetBottom(my_buttons[ind_of_button]);
-                    Canvas.SetBottom(my_buttons[ind_of_button], b);
-                    Canvas.SetLeft(my_buttons[ind_of_button], a);
-                } else
-                {
-                    empty_pos[ind_of_empty_pos].x = Canvas.GetLeft(other_buttons[ind_of_button]);
-                    empty_pos[ind_of_empty_pos].y = Canvas.GetBottom(other_buttons[ind_of_button]);
-                    Canvas.SetBottom(other_buttons[ind_of_button], b);
-                    Canvas.SetLeft(other_buttons[ind_of_button], a);
-                }
-                
-            });
-            await Task.Delay(105);
+                    if (my)
+                    {
+                        empty_pos[ind_of_empty_pos].x = Canvas.GetLeft(my_buttons[ind_of_button]);
+                        empty_pos[ind_of_empty_pos].y = Canvas.GetBottom(my_buttons[ind_of_button]);
+                        Canvas.SetBottom(my_buttons[ind_of_button], b);
+                        Canvas.SetLeft(my_buttons[ind_of_button], a);
+                        if (inf.eat)
+                        {
+
+                            empty_pos.Add(new Position(Canvas.GetLeft(other_buttons[inf.other_but_ind]),
+                                Canvas.GetBottom(other_buttons[inf.other_but_ind])));
+                            other_buttons[inf.other_but_ind].Visibility = Visibility.Hidden;
+                            other_buttons.RemoveAt(inf.other_but_ind);
+                        }
+                    }
+                    else
+                    {
+                        empty_pos[ind_of_empty_pos].x = Canvas.GetLeft(other_buttons[ind_of_button]);
+                        empty_pos[ind_of_empty_pos].y = Canvas.GetBottom(other_buttons[ind_of_button]);
+                        Canvas.SetBottom(other_buttons[ind_of_button], b);
+                        Canvas.SetLeft(other_buttons[ind_of_button], a);
+                    }
+
+                });
+                await Task.Delay(105);
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            
 
         }
         async Task FindPossibleButtonIndsForMove()
@@ -276,38 +306,65 @@ namespace Bot
                             y = Canvas.GetBottom(my_buttons[i]);
                             double empty_x = empty_pos[j].x;
                             double empty_y = empty_pos[j].y;
-                            // просто шаг на пустую клетку
+// просто шаг на пустую клетку
+                      //      MessageBox.Show("m " +i + " "+ empty_x + " " + empty_y + " " + x + " " + y);
                             if (x == empty_x && (y - height_step == empty_y || y + height_step == empty_y))
                             {
-                                enabled_moves.Add(new InfoForMove(j, i));
+                                enabled_moves.Add(new InfoForMove(j, i, false));
                                 a += i;
                             }
-                            else if (y == empty_y && (x - width_step == empty_x || x + width_step == empty_x))
+                          else if (y == empty_y && (x - width_step == empty_x || x + width_step == empty_x))
                             {
-                                enabled_moves.Add(new InfoForMove(j, i));
+                                enabled_moves.Add(new InfoForMove(j, i, false));
                                 a += i;
                             }
-                            else if (x + width_step == empty_x && (y - height_step == empty_y | y + height_step == empty_y))
+                           else if (x + width_step == empty_x && (y - height_step == empty_y | y + height_step == empty_y))
                             {
-                                enabled_moves.Add(new InfoForMove(j, i));
+                                enabled_moves.Add(new InfoForMove(j, i, false));
                                 a += i;
                             }
                             else if (y + height_step == empty_y && (x - width_step == empty_x | x + width_step == empty_x))
                             {
-                                enabled_moves.Add(new InfoForMove(j, i));
+                                enabled_moves.Add(new InfoForMove(j, i, false));
                                 a += i;
                             }
                             else if (x - width_step == empty_x && (y - height_step == empty_y | y + height_step == empty_y))
                             {
-                                enabled_moves.Add(new InfoForMove(j, i));
+                                enabled_moves.Add(new InfoForMove(j, i, false));
                                 a += i;
                             }
                             else if (y - height_step == empty_y && (x - width_step == empty_x | x + width_step == empty_x))
                             {
-                                enabled_moves.Add(new InfoForMove(j, i));
+                                enabled_moves.Add(new InfoForMove(j, i, false));
                                 a += i;
                             }
                             // съесть чужую фишку
+                            if(x == empty_x && y + 2 * height_step == empty_y)
+                            {
+                                bool ok = false;
+                                int res_ind = 0;
+                                for (int k = 0; !ok && k < other_buttons.Count; k++)
+                                {
+                                    if (Canvas.GetLeft(other_buttons[k]) == x && Canvas.GetBottom(other_buttons[k]) + height_step == y)
+                                    {
+                                        ok = true;
+                                        res_ind = k;
+                                    }
+                                }
+                                if(ok)
+                                {
+                                    enabled_moves.Add(new InfoForMove(j, i, true, res_ind));
+                                    MessageBox.Show(j + " " + i + " " + true + " " + res_ind);
+                                } /*else
+                                {
+                                    string cur = "";
+                                    for (int k = 0; !ok && k < other_buttons.Count; k++)
+                                    {
+                                        cur += k + " " + i+ " "  + Canvas.GetBottom(other_buttons[k]) + height_step + "\n";
+                                    }
+                                   // MessageBox.Show(cur);
+                                }*/
+                            }
                         }
                     }
                   //  MessageBox.Show(a);
